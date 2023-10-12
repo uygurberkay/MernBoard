ebea8e91e792fd71d0f63a645112b60fb4254f6f
uygurberkay <50241715+uygurberkay@users.noreply.github.com>
2023-10-12 07:59:42 +0300

    admin-page-done

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Logo } from './Logo';
 export { default as Navbar } from './Navbar';
 export { default as FormRow } from './FormRow.jsx';
+export { default as StatItem } from './StatItem.jsx'
 export { default as NavLinks } from './NavLinks.jsx';
 export { default as BigSideBar } from './BigSideBar';
 export { default as ThemeToggle } from './ThemeToggle.jsx';
